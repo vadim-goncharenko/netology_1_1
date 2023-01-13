df2127a6ddf709aa2cb03e84f7ada36afd293581
vadim-goncharenko <elextasisylabelleza@gmail.com>
2023-01-13 14:16:20 +0300

    1

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,30 @@
+import React  from 'react';
+import './ShopItemFunc/css/main.css';
+import PropTypes from 'prop-types';
+
+function ShopItemFunc(props) {    
+    const item = props.item;
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">
+                {item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+      )
+}
+
+ShopItemFunc.propTypes  = {
+    //item: PropTypes.instanceOf(ShopItemModel).isRequired
+    item: PropTypes.object
+}
+
+export default ShopItemFunc;
